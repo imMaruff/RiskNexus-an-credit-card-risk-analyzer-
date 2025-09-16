ae12f8a9a90e6a9b660204b24f8eaf1d428e1ac4
Maruf <maruf.khann931@gmail.com>
2025-09-16 16:19:27 +0530

    Update some errors

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQw9pxHTewjR_2bJrypEm3m-DBMmMer1o",
-  authDomain: "credit-risk-analysis-c8cfe.firebaseapp.com",
-  projectId: "credit-risk-analysis-c8cfe",
-  storageBucket: "credit-risk-analysis-c8cfe.firebasestorage.app",
-  messagingSenderId: "44076403096",
-  appId: "1:44076403096:web:082caabcd6fb6b864c03ed",
-  measurementId: "G-ZMZF285CED"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
